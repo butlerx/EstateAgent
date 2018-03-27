c90690a85e22d0bae0169859ff54eb9048315367
butlerx <butlerx@notthe.cloud>
2018-03-27 15:43:29 +0100

    add client

---
+++ src/main/java/com/dcu/client/App.java
@@ -0,0 +1,29 @@
+package com.dcu.client;
+
+import com.dcu.Property;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class App {
+
+  private static final String REST_URI = "http://localhost:8080/api/property";
+  private Client client = ClientBuilder.newClient();
+
+  public Response createProperty(Property prop) {
+    return client
+        .target(REST_URI)
+        .request(MediaType.APPLICATION_JSON)
+        .post(Entity.entity(prop, MediaType.APPLICATION_JSON));
+  }
+
+  public Property getProperty(int id) {
+    return client
+        .target(REST_URI)
+        .path(String.valueOf(id))
+        .request(MediaType.APPLICATION_JSON)
+        .get(Property.class);
+  }
+}
